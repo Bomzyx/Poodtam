43c69092dfc963b5e5d9f21d04dd7ab25b27d208
Chanawat Thuasuphap <earthza991@hotmail.com>
2023-03-07 01:49:40 +0700

    Format app.py with black

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from mongoengine import connect
 app = Flask(__name__)
 
 # Create a connection to the MongoDB database
-connect('mydatabase', host='mongodb://localhost:27017/')
+connect("poodtam-mongodb", host="mongodb://localhost:27017/")
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    return "Hello, World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
