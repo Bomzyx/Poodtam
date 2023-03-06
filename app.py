39e471a00b7ff2bc79a6662355b443f6cfb2cd24
Chanawat Thuasuphap <earthza991@hotmail.com>
2023-03-07 01:44:41 +0700

    Create simple Flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from mongoengine import connect
+
+app = Flask(__name__)
+
+# Create a connection to the MongoDB database
+connect('mydatabase', host='mongodb://localhost:27017/')
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
